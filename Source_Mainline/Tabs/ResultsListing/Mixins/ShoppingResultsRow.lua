f2cba7b045cf03b9b2dac84df5b7a437e916f945
Murilo Mielke <murilo.eng@gmail.com>
2024-09-30 09:08:02 -0300

    adding right-click buying to commodities

--- Source_Mainline/Tabs/ResultsListing/Mixins/ShoppingResultsRow.lua
+++ Source_Mainline/Tabs/ResultsListing/Mixins/ShoppingResultsRow.lua
@@ -1,5 +1,39 @@
 AuctionatorShoppingResultsRowMixin = CreateFromMixins(AuctionatorResultsRowTemplateMixin)
 
+local function debugtable(tab) 
+  print("=====================================")
+  print("SIZE:", #tab)
+  print("{")
+  if not tab then
+    print("nil")
+  else
+    for k,v in pairs(tab) do
+      print('"'..k..'" = '..tostring(v))
+    end
+  end
+  print("}")
+  print("=====================================")
+end
+
+local item = {}
+
+function PurchaseCommodity(itemID, quantity)
+	C_AuctionHouse.StartCommoditiesPurchase(itemID, quantity)
+	item.itemID = itemID
+	item.quantity = quantity
+end
+
+local function PurchaseCommodityOnEvent(self, event)
+	if next(item) then
+		C_AuctionHouse.ConfirmCommoditiesPurchase(item.itemID, item.quantity)
+		wipe(item)
+	end
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("AUCTION_HOUSE_THROTTLED_SYSTEM_READY")
+f:SetScript("OnEvent", PurchaseCommodityOnEvent)
+
 function AuctionatorShoppingResultsRowMixin:OnClick(button, ...)
   Auctionator.Debug.Message("AuctionatorShoppingResultsRowMixin:OnClick()")
 
@@ -7,6 +41,15 @@ function AuctionatorShoppingResultsRowMixin:OnClick(button, ...)
     AuctionHouseBrowseResultsFrameMixin.OnBrowseResultSelected({}, self.rowData)
 
   elseif button == "RightButton" then
+    if C_AuctionHouse.GetItemKeyInfo(self.rowData.itemKey) then
+      local itemKeyInfo = C_AuctionHouse.GetItemKeyInfo(self.rowData.itemKey)
+      local quantity = self.rowData.purchaseQuantity or 1
+      if itemKeyInfo.isCommodity then
+        PurchaseCommodity(itemKeyInfo.itemID, quantity)
+        return
+      end
+    end
+
     Auctionator.EventBus
       :RegisterSource(self, "ShoppingResultsRowMixin")
       :Fire(self, Auctionator.Shopping.Tab.Events.ShowHistoricalPrices, self.rowData)
